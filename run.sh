d2d1d2fea30a456333634879c69b920beceb78c7
k33g <k33g@zeiracorp.local>
2017-06-29 06:29:34 +0200

    🚀 first version

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java  -jar target/vertx-service-discovery-backend-http-1.0-SNAPSHOT.jar
---
+++ src/main/java/org/typeunsafe/HttpBackendService.java
@@ -0,0 +1,7 @@
+package org.typeunsafe;
+
+public class HttpBackendService {
+  public static void main(String [] args) {
+    System.out.println("Hello 🌍");
+  }
+}
